6b428de4ea4f25b258f5988bf18a261d18087ea3
HortusDev <hortusdev@gmail.com>
2025-05-05 13:27:53 +0300

    Update MVP
    updating mvp

--- H-PDF Converter/MainWindow.xaml.cs
+++ H-PDF Converter/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ using Path = System.IO.Path;
 
 namespace H_PDF_Converter
 {
-    //[System.Runtime.InteropServices.ComVisible(true)] // to merge VBA and c#
+    //[System.Runtime.InteropServices.ComVisible(true)] // if need to merge VBA into c#
     //[System.Runtime.InteropServices.ClassInterface(
     //System.Runtime.InteropServices.ClassInterfaceType.None)]
     /// <summary>
@@ -34,15 +34,12 @@ namespace H_PDF_Converter
             InitializeComponent();
         }
 
-        private void SelectFileClick(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Действие выполнено");
-        }
-
-        //private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //private void SelectFileClick(object sender, RoutedEventArgs e)
         //{
-
+        //    MessageBox.Show("Done");
         //}
+
+        
         private void UpdateFilePath(string path) //updating file path
         {
             FilePath = path;
@@ -65,30 +62,12 @@ namespace H_PDF_Converter
                 MessageBox.Show("Select a file");
                 return;
             }
-
-            //switch (conversionDropDown.SelectedIndex)
-            //{
-            //    //case 0: // Convert PDF to Doc
-            //    //    ConvertToDoc(FilePath);
-            //    //    break;
-            //    //    case 1:
-            //    //        // Convert PDFTODOC
-            //    //        ConvertPDFtoDoc(pathTextBox.Text);
-            //    //        break;
-            //    //    case 2:
-            //    //        ConvertPNGToPDF(pathTextBox.Text);
-            //    //        break;
-            //    //default:
-            //    //    MessageBox.Show("Select an option");
-            //    //    return;
-            //}
-            
         }
 
         private void ConvertToDoc(object sender, RoutedEventArgs e)
         {
             string inpFile = FilePath;
-            string outFile = @"D:\Файлы\Result228.docx";
+            string outFile = FilePath + ".docx";
 
             // Specifying PdfLoadOptions we explicitly set that a loadable document is PDF.
             PdfLoadOptions pdfLO = new PdfLoadOptions()
@@ -117,7 +96,7 @@ namespace H_PDF_Converter
         private void ConvertToJPEG(object sender, RoutedEventArgs e)
         {
             string inpFile = FilePath;
-            string outFile = @"D:\Файлы\Result5.jpeg";
+            string outFile = FilePath + ".jpeg";
 
             DocumentCore dc = DocumentCore.Load(inpFile);
 
@@ -134,7 +113,7 @@ namespace H_PDF_Converter
         private void ConvertToPnG(object sender, RoutedEventArgs e)
         {
             string inpFile = FilePath;
-            string outFile = @"D:\Файлы\ResultPNG.png";
+            string outFile = FilePath + ".png";
 
             DocumentCore dc = DocumentCore.Load(inpFile);
 
@@ -150,127 +129,11 @@ namespace H_PDF_Converter
         private void ConvertToTXT(object sender, RoutedEventArgs e)
         {
             string inpFile = FilePath;
-            string outFile = @"D:\Файлы\ResultText.txt";
+            string outFile = FilePath + "txt";
 
             DocumentCore dc = DocumentCore.Load(inpFile);
             dc.Save(outFile);
-            // Important for Linux: Install MS Fonts
-            // sudo apt install ttf-mscorefonts-installer -y
-
-            // Open the result for demonstration purposes.
             System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(outFile) { UseShellExecute = true });
-        }
-        //    private void ConvertDocToPDF(string docPath)
-        //    {
-        //        WordDocument wordDocument = new WordDocument(docPath, FormatType.Automatic);
-        //        DocToPDFConverter converter = new DocToPDFConverter();
-        //        PdfDocument pdfDocument = converter.ConvertToPDF(wordDocument);
-
-        //        string newPDFPath = docPath.Split('.')[0] + ".pdf";
-        //        pdfDocument.Save(newPDFPath);
-
-        //        pdfDocument.Close(true);
-        //        wordDocument.Close();
-        //    }
-
-
-        //    private void ConvertPNGToPDF(string pngPath)
-        //    {
-        //        PdfDocument pdfDoc = new PdfDocument();
-        //        PdfImage pdfImage = PdfImage.FromStream(new FileStream(pngPath, FileMode.Open));
-        //        PdfPage pdfPage = new PdfPage();
-        //        PdfSection pdfSection = pdfDoc.Sections.Add();
-        //        pdfSection.Pages.Insert(0, pdfPage);
-        //        pdfPage.Graphics.DrawImage(pdfImage, 0, 0);
-
-        //        string newPNGPath = pngPath.Split('.')[0] + ".pdf";
-        //        pdfDoc.Save(newPNGPath);
-        //        pdfDoc.Close(true);
-        //    }
-
-        //    private void ConvertPDFtoDoc(string pdfPath)
-        //    {
-        //        WordDocument wordDocument = new WordDocument();
-        //        IWSection section = wordDocument.AddSection();
-        //        section.PageSetup.Margins.All = 0;
-        //        IWParagraph firstParagraph = section.AddParagraph();
-
-        //        SizeF defaultPageSize = new SizeF(wordDocument.LastSection.PageSetup.PageSize.Width,
-        //            wordDocument.LastSection.PageSetup.PageSize.Height);
-
-        //        using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(pdfPath))
-        //        {
-        //            for (int i = 0; i < loadedDocument.Pages.Count; i++)
-        //            {
-        //                using (var image = loadedDocument.ExportAsImage(i, defaultPageSize, false))
-        //                {
-        //                    IWPicture picture = firstParagraph.AppendPicture(image);
-        //                    picture.Width = image.Width;
-        //                    picture.Height = image.Height;
-        //                }
-        //            }
-        //        }
-        //        ;
-
-        //        string newPDFPath = pdfPath.Split('.')[0] + ".docx";
-        //        wordDocument.Save(newPDFPath);
-        //        wordDocument.Dispose();
-
-        //    }
-
-
-
-        //    private void SelectFile_Click(object sender, RoutedEventArgs e)
-        //    {
-        //        OpenFileDialog openFileDialog = new OpenFileDialog();
-        //        bool? result = openFileDialog.ShowDialog();
-
-        //        if (result.HasValue && result.Value)
-        //        {
-        //            pathTextBox.Text = openFileDialog.FileName;
-        //        }
-        //    }
-
-        //    private void OpenFolder(string folderPath)
-        //    {
-        //        ProcessStartInfo startInfo = new ProcessStartInfo()
-        //        {
-        //            Arguments = folderPath.Substring(0, folderPath.LastIndexOf('\\')),
-        //            FileName = "explorer.exe"
-        //        };
-        //        Process.Start(startInfo);
-
-        //    }
-
-
-        //    private void myMainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
-        //    {
-        //        {
-        //            myMainWindow.Width = e.NewSize.Width;
-        //            myMainWindow.Height = e.NewSize.Height;
-
-        //            double xChange = 1, yChange = 1;
-
-        //            if (e.PreviousSize.Width != 0)
-        //                xChange = (e.NewSize.Width / e.PreviousSize.Width);
-
-        //            if (e.PreviousSize.Height != 0)
-        //                yChange = (e.NewSize.Height / e.PreviousSize.Height);
-
-        //            foreach (FrameworkElement fe in myGrid.Children)
-        //            {
-        //                if (fe is Grid == false)
-        //                {
-        //                    fe.Height = fe.ActualHeight * yChange;
-        //                    fe.Width = fe.ActualWidth * xChange;
-
-        //                    Canvas.SetTop(fe, Canvas.GetTop(fe) * yChange);
-        //                    Canvas.SetLeft(fe, Canvas.GetLeft(fe) * xChange);
-
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
+        } 
     }
 }
